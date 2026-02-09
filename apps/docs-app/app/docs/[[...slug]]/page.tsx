66bd8065018768f4144e8af387a3162bfda28b14
Arun Patra <arun@astronuts.io>
2026-02-09 22:13:10 +0530

    fix

--- apps/docs-app/app/docs/[[...slug]]/page.tsx
+++ apps/docs-app/app/docs/[[...slug]]/page.tsx
@@ -67,7 +67,7 @@ export default async function Page(props: PageProps) {
         <LLMCopyButton markdownUrl={`${page.url}.mdx`} />
         <ViewOptions
           markdownUrl={`${page.url}.mdx`}
-          githubUrl={`https://github.com/journium/journium-docs/blob/main/content/docs/${page.path}`}
+          githubUrl={`https://github.com/journium/journium-docs/blob/main/apps/docs-app/content/docs/${page.path}`}
         />
       </div>
       <DocsBody>
